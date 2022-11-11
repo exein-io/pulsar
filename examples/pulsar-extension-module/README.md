065aada72e88e6f341508895a580b4227e8fd0b7
Matteo Nardi <matteo@exein.io>
2022-11-11 11:04:40 +0100

    docs(examples): add pulsar-extension-module

---
+++ examples/pulsar-extension-module/README.md
@@ -0,0 +1,39 @@
+# Extending Pulsar with custom modules
+
+The best way to extend Pulsar with custom behavior is to write a custom module.
+This is great for:
+- Extracting new events from the kernel
+- Inspecting existing events and identifying security threats
+- Sending these events to a remote server
+
+```
+$ cargo build && sudo ./target/debug/pulsar-extension-module pulsard
+```
+
+By default our custom module will do nothing, but we can instruct it to warn
+about DNS queries to `exein.io`.
+```
+pulsar config --set my-custom-module.forbidden_dns=exein.io
+dig exein.io
+```
+
+The commands above will produce this output in the Pulsar daemon:
+```
+Configuration changed: MyModuleConfig { print_events: false, forbidden_dns: Some("exein.io") }
+Event { header: Header { pid: 28745, is_threat: true, source: ModuleName("network-monitor"), timestamp: SystemTime { tv_sec: 1668160509, tv_ns
+ec: 818640866 }, image: "/usr/bin/dig", parent: 24586, fork_time: SystemTime { tv_sec: 1668160509, tv_nsec: 813495081 } }, payload: AnomalyDet
+ection { score: 1.0 } }
+```
+
+## How to do it
+
+We'll add a dependency on the main Pulsar binary and launch it with the custom
+module we wrote.
+
+```
+pulsar = { git = "https://github.com/Exein-io/pulsar", rev = "797f68641ed92b35b152e0d147b9cdcf3bfa49e5" }
+pulsar-core = { git = "https://github.com/Exein-io/pulsar", rev = "797f68641ed92b35b152e0d147b9cdcf3bfa49e5" }
+tokio = { version = "1", features = ["full"] }
+```
+
+Check the [main file](./src/main.rs) and the [custom module](./src/my_custom_module.rs) we're using in this example.
---
+++ examples/pulsar-extension-module/src/my_custom_module.rs
@@ -0,0 +1,85 @@
+//! A showcase of what a Pulsar modules can do
+use std::sync::Arc;
+
+use pulsar_core::pdk::{
+    CleanExit, ConfigError, Event, ModuleConfig, ModuleContext, ModuleError, Payload, PulsarModule,
+    ShutdownSignal, Version,
+};
+
+const MODULE_NAME: &str = "my-custom-module";
+
+pub fn module() -> PulsarModule {
+    PulsarModule::new(MODULE_NAME, Version::new(0, 1, 0), module_task)
+}
+
+async fn module_task(
+    // ModuleContext allows our module to access the agent features
+    ctx: ModuleContext,
+    // ShutdownSignal will be used by the agent to stop this module
+    mut shutdown: ShutdownSignal,
+) -> Result<CleanExit, ModuleError> {
+    // Get a receiver for all module events.
+    let mut receiver = ctx.get_receiver();
+
+    // Get the configuration of your own module
+    let mut rx_config = ctx.get_cfg::<MyModuleConfig>();
+    let mut config = rx_config.borrow().clone()?;
+
+    // Get a channel for sending new events
+    let sender = ctx.get_sender();
+
+    loop {
+        tokio::select! {
+            // Handle configuration changes:
+            _ = rx_config.changed() => {
+                config = rx_config.borrow().clone()?;
+                println!("Configuration changed: {config:?}");
+            }
+
+            // Receive events from other modules:
+            event = receiver.recv() => {
+                let event: Arc<Event> = event?;
+                if config.print_events {
+                    println!("{event:?}");
+                }
+                // Identify events as threats:
+                if let Some(forbidden_dns) = &config.forbidden_dns {
+                    if let Payload::DnsQuery { questions } = &event.payload {
+                        if questions.iter().any(|question| &question.name == forbidden_dns) {
+                            let mut event = (*event).clone();
+                            event.header.is_threat = true;
+                            sender.send_derived_event(
+                                &event,
+                                Payload::AnomalyDetection { score: 1.0 }
+                            );
+                        }
+                    }
+                }
+            },
+
+            // Stop when receiving the shudown signal:
+            r = shutdown.recv() => return r,
+        }
+    }
+}
+
+/// An exmaple configuration. You can put whatever you want here,
+/// as long as you implement `TryFrom<&ModuleConfig>`
+#[derive(Clone, Debug, Default)]
+struct MyModuleConfig {
+    /// Enable printing events to stdout
+    print_events: bool,
+    /// Generate a threat event when resolving this domain name
+    forbidden_dns: Option<String>,
+}
+
+impl TryFrom<&ModuleConfig> for MyModuleConfig {
+    type Error = ConfigError;
+
+    fn try_from(config: &ModuleConfig) -> Result<Self, Self::Error> {
+        Ok(MyModuleConfig {
+            print_events: config.with_default("print_events", false)?,
+            forbidden_dns: config.get_raw("forbidden_dns").map(String::from),
+        })
+    }
+}
