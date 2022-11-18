974b18186d15e2159e3a6c0db20ea94de78b1afb
Matteo Nardi <matteo@exein.io>
2022-11-18 09:16:30 +0100

    misc(test-suite): add list of modules to lib.rs
    The reason for this change is to allow external distributions of pulsar
    (with custom modules, see examples/pulsar-extension-module/) to define
    their tests *on top* of the existing test-suite.

--- test-suite/src/lib.rs
+++ test-suite/src/lib.rs
@@ -1,3 +1,14 @@
 mod test_suite_runner;
 
 pub use test_suite_runner::TestSuiteRunner;
+
+/// Returns the testable modules of Pulsar
+pub fn modules() -> Vec<bpf_common::test_runner::TestSuite> {
+    vec![
+        bpf_common::feature_autodetect::test_suite::tests(),
+        file_system_monitor::test_suite::tests(),
+        network_monitor::test_suite::tests(),
+        process_monitor::test_suite::tests(),
+        syscall_monitor::test_suite::tests(),
+    ]
+}
--- test-suite/src/main.rs
+++ test-suite/src/main.rs
@@ -1,14 +1,6 @@
-use test_suite::TestSuiteRunner;
+use test_suite::{modules, TestSuiteRunner};
 
 #[tokio::main]
 async fn main() {
-    TestSuiteRunner::spawn()
-        .run_tests(vec![
-            bpf_common::feature_autodetect::test_suite::tests(),
-            file_system_monitor::test_suite::tests(),
-            network_monitor::test_suite::tests(),
-            process_monitor::test_suite::tests(),
-            syscall_monitor::test_suite::tests(),
-        ])
-        .await;
+    TestSuiteRunner::spawn().run_tests(modules()).await;
 }
