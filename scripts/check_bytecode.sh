2d8c25006a4ee968bef4c77e0e32b34877ba1fc0
Matteo Nardi <matteo@exein.io>
2023-01-17 11:35:15 +0100

    new(scripts): helper to inspect eBPF bytecode
    Add a script which compiles a probe and uses llvm-objdump to output
    its annotated bytecode. This is useful during development to understand
    what compiler optimizations are happening and possible verifier
    complaints.
    
    Example:
    ```
    ./scripts/check_bytecode.sh modules/file-system-monitor/probe.bpf.c
    ```

---
+++ scripts/check_bytecode.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Compile an eBPF source and print its bytecode. This is useful to understand
+# verifier issues.
+
+# Print commands
+set -x
+# Stop on error
+set -e
+
+# Compile eBPF to /tmp/obj.o
+clang -g -O2 -c $1 -o /tmp/obj.o -target bpf -D__TARGET_ARCH_x86 -I bpf-common/include/ -I bpf-common/include/x86_64/
+sleep 1
+llvm-objdump -S --no-show-raw-insn /tmp/obj.o
