57db994b0fdd1a77e5f5fd545b3178030fa1f92f
banditopazzo <banditopazzo@gmail.com>
2023-01-19 11:50:32 +0100

    refactor(xtask): cli with clap; add release flag to commands

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,43 +1,39 @@
-use std::env::{self, Args};
+use clap::Parser;
+use run::run_with_sudo;
 
-use anyhow::Result;
-use xshell::{cmd, Shell};
+mod run;
+
+#[derive(Debug, Parser)]
+#[clap(disable_help_subcommand = true)]
+pub struct Options {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Parser)]
+enum Command {
+    /// Run pulsar daemon with admin privileges
+    Pulsard(run::Options),
+    /// Run pulsar cli with admin privileges
+    Pulsar(run::Options),
+    /// Run a single module with admin privileges
+    Probe(run::Options),
+    /// Run eBPF test suite with admin privileges
+    Test(run::Options),
+}
 
 fn main() {
-    if let Err(e) = try_main() {
+    let opts = Options::parse();
+
+    let ret = match opts.command {
+        Command::Pulsard(opts) => run_with_sudo("pulsar-exec", &["pulsard"], opts),
+        Command::Pulsar(opts) => run_with_sudo("pulsar-exec", &["pulsar"], opts),
+        Command::Probe(opts) => run_with_sudo("probe", &[], opts),
+        Command::Test(opts) => run_with_sudo("test-suite", &[], opts),
+    };
+
+    if let Err(e) = ret {
         eprintln!("{}", e);
-        std::process::exit(-1);
+        std::process::exit(1);
     }
-}
-
-fn try_main() -> Result<()> {
-    let mut args = env::args();
-    let task = args.nth(1);
-    match task.as_deref() {
-        Some("test") => run_with_sudo("test-suite", &[], args)?,
-        Some("pulsard") => run_with_sudo("pulsar-exec", &["pulsard"], args)?,
-        Some("pulsar") => run_with_sudo("pulsar-exec", &["pulsar"], args)?,
-        Some("probe") => run_with_sudo("probe", &[], args)?,
-        _ => print_help(),
-    }
-    Ok(())
-}
-
-fn print_help() {
-    eprintln!(
-        "Tasks:
-test            run eBPF test suite with admin privileges
-pulsard         run pulsar agent daemon with admin privileges
-pulsar          run pulsar agent client with admin privileges
-probe           run a single module with admin privileges
-"
-    )
-}
-
-fn run_with_sudo(binary: &str, prefix: &[&str], args: Args) -> Result<()> {
-    let sh = Shell::new()?;
-    cmd!(sh, "cargo build --bin {binary}").run()?;
-    // -E preserves the environment
-    cmd!(sh, "sudo -E ./target/debug/{binary} {prefix...} {args...}").run()?;
-    Ok(())
 }
