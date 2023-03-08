2f8ed429476d43fc6baba1dea6777db04c60c451
Matteo Nardi <matteo@exein.io>
2023-03-08 09:45:18 +0100

    misc(xtask): use flattened struct for CLI parsing options

--- xtask/src/cross.rs
+++ xtask/src/cross.rs
@@ -4,18 +4,6 @@ use xshell::{cmd, Shell};
 
 #[derive(Debug, Parser)]
 pub(crate) struct Options {
-    /// Target architecture
-    #[clap(long, default_value = "x86_64-unknown-linux-musl")]
-    target: String,
-
-    /// Binary to compile
-    #[clap(long, default_value = "test-suite")]
-    binary: String,
-
-    /// Build and run the release target
-    #[clap(long)]
-    release: bool,
-
     #[clap(subcommand)]
     command: Command,
 }
@@ -27,37 +15,68 @@ enum Command {
         /// Where to copy the file
         #[clap(long, default_value = "/tmp/")]
         destination: String,
+
+        #[command(flatten)]
+        opts: SharedOptions,
     },
     /// Run over the given SSH connection.
     Run {
         /// Target ssh
         #[clap(long, default_value = "qemu")]
         ssh_target: String,
+
         /// Arguments passed on process execution
         args: Vec<String>,
+
+        #[command(flatten)]
+        opts: SharedOptions,
     },
+}
+
+#[derive(Debug, Parser)]
+struct SharedOptions {
+    /// Target architecture
+    #[clap(long, default_value = "x86_64-unknown-linux-musl")]
+    target: String,
+
+    /// Binary to compile
+    #[clap(long, default_value = "test-suite")]
+    binary: String,
+
+    /// Build and run the release target
+    #[clap(long)]
+    release: bool,
 }
 
 pub(crate) fn run(options: Options) -> Result<()> {
     let sh = Shell::new()?;
-    let Options {
+    let SharedOptions {
         target,
         binary,
         release,
-        command,
-    } = options;
-    let args = if release { Some("--release") } else { None };
+    } = match &options.command {
+        Command::Build { opts, .. } => opts,
+        Command::Run { opts, .. } => opts,
+    };
+    let args = if *release { Some("--release") } else { None };
     cmd!(
         sh,
         "cross build --target {target} --target-dir target/cross --workspace --bin {binary} {args...}"
     )
     .run()?;
-    let build_type = if release { "release" } else { "debug" };
+    let build_type = if *release { "release" } else { "debug" };
     let binary_file = format!("target/cross/{target}/{build_type}/{binary}");
     cmd!(sh, "llvm-strip {binary_file}").run()?;
-    match command {
-        Command::Build { destination } => cmd!(sh, "cp {binary_file} {destination}").run()?,
-        Command::Run { ssh_target, args } => {
+    match &options.command {
+        Command::Build {
+            destination,
+            opts: _,
+        } => cmd!(sh, "cp {binary_file} {destination}").run()?,
+        Command::Run {
+            ssh_target,
+            args,
+            opts: _,
+        } => {
             cmd!(sh, "scp {binary_file} {ssh_target}:/tmp/").run()?;
             cmd!(sh, "ssh {ssh_target} /tmp/{binary} {args...}").run()?;
         }
