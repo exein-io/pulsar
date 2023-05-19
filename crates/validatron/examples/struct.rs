35f56290446c2ffd1903f8e11c81668eb6217808
banditopazzo <banditopazzo@gmail.com>
2023-05-19 14:12:02 +0200

    refactor: use macro in the example

--- crates/validatron/examples/struct.rs
+++ crates/validatron/examples/struct.rs
@@ -2,26 +2,15 @@ use std::error::Error;
 
 use validatron::{
     validator::get_valid_rule, Field, Match, Operator, RelationalOperator, Validatron,
-    ValidatronClass,
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Validatron)]
 pub struct MyStruct {
     pub a: i32,
     pub b: i32,
     pub c: i32,
 }
 
-impl Validatron for MyStruct {
-    fn get_class() -> ValidatronClass {
-        Self::class_builder()
-            .struct_class_builder()
-            .add_field("a", Box::new(|t| &t.a))
-            .add_field("b", Box::new(|t| &t.b))
-            .add_field("c", Box::new(|t| &t.c))
-            .build()
-    }
-}
 /// Match the field "a" against the fixed value "111" using the "Equals" operator
 fn fixed_match(test: &MyStruct) -> Result<bool, Box<dyn Error>> {
     let rule = get_valid_rule::<MyStruct>(
