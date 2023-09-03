1c0f6cf1fa172a770b2d742464290dc1f5fe4e6e
Mike Rostecki <vadorovsky@protonmail.com>
2023-09-03 17:55:29 +0200

    fix(bpf): Handle the post-6.4 definition of `iov_iter`
    Kernel v6.4 introduced a change in the layout of `struct iov_iter`
    (torvalds/linux@747b1f6). Handle it accordingly in `on_socket_sendmsg`
    and `on_socket_recvmsg` BPF programs.
    
    Fixes: #194

---
+++ crates/bpf-builder/include/iov_iter_compat.h
@@ -0,0 +1,30 @@
+#include "vmlinux.h"
+
+/*
+ * `iov_iter` definition from kernel <= 6.4.
+ *
+ * Unfortunately, it was redefined in a change included in 6.4[0] which is not
+ * backwards-compatible and impossible to handle just with BTF. This struct
+ * is defined to handle the old definition.
+ */
+struct iov_iter_compat {
+	u8 iter_type;
+	bool data_source;
+	size_t iov_offset;
+	size_t count;
+	union {
+		const struct iovec *iov;
+		const struct kvec *kvec;
+		const struct bio_vec *bvec;
+		struct xarray *xarray;
+		struct pipe_inode_info *pipe;
+	};
+	union {
+		long unsigned int nr_segs;
+		struct {
+			unsigned int head;
+			unsigned int start_head;
+		};
+		loff_t xarray_start;
+	};
+};
