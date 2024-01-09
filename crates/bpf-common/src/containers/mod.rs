8d45f96ae61f024250b2ef9cb08309d0724d5c4c
banditopazzo <banditopazzo@gmail.com>
2024-01-09 17:08:20 +0100

    feat(bpf-common): add boltdb support to libpod container id resolve

--- crates/bpf-common/src/containers/mod.rs
+++ crates/bpf-common/src/containers/mod.rs
@@ -1,21 +1,24 @@
 use std::{
+    ffi::{CStr, OsString},
     fmt,
     fs::File,
-    io::{self, BufRead, BufReader},
-    path::PathBuf,
+    io::{self, BufReader},
+    mem,
+    os::unix::ffi::OsStringExt,
+    path::{Path, PathBuf},
+    ptr,
 };
 
 use diesel::prelude::*;
+use ini::Ini;
+use nix::unistd::{Pid, Uid};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use validatron::Validatron;
 
-pub mod schema;
+use crate::parsing::procfs::{self, ProcfsError};
 
-const ETC_PASSWD: &str = "/etc/passwd";
-const DOCKER_CONTAINERS_PATH: &str = "/var/lib/docker/containers";
-const LIBPOD_DB_ROOT_PATH: &str = "/var/lib/containers/storage/db.sql";
-const LIBPOD_IMAGE_STORE_PATH: &str = "/var/lib/containers/storage/overlay-images/images.json";
+pub mod schema;
 
 #[derive(Error, Debug)]
 pub enum ContainerError {
@@ -40,19 +43,41 @@ pub enum ContainerError {
     #[error("path `{path}` is non-UTF-8")]
     PathNonUtf8 { path: PathBuf },
     #[error("could not connect to the database `{path:?}`")]
-    DatabaseConnection {
+    SqliteConnection {
         #[source]
         source: ConnectionError,
         path: PathBuf,
     },
     #[error("could not find libpod container `{id}`")]
     ContainerNotFound { id: String },
+    #[error("could not find libpod image store")]
+    ImageStoreNotFound,
     #[error("could not find container image `{id}` in `{path:?}`")]
     ImageNotFound { id: String, path: PathBuf },
     #[error("parsing image digest {digest} failed")]
     ParseDigest { digest: String },
     #[error("invalid hash function {hash_fn}")]
     InvalidHashFunction { hash_fn: String },
+    #[error(transparent)]
+    Procfs(#[from] ProcfsError),
+    #[error("error parsing libpod configuration from {path}")]
+    LibpodConfParsing {
+        #[source]
+        source: ini::Error,
+        path: PathBuf,
+    },
+    #[error("unknown libpod database backend {0}")]
+    UnknownLibpodDatabase(String),
+    #[error("home directory not found for user with uid: {uid}")]
+    HomeDirNotFound { uid: Uid },
+    #[error("podman db `{0}` not found")]
+    LibpodDBNotFound(String),
+    #[error("bolt db `{0}` open failed")]
+    BoltDBOpenFailed(String),
+    #[error("bolt bucket `{0}` not found")]
+    BoltBucketNotFound(String),
+    #[error("bolt key `{0}` not found")]
+    BoltKeyNotFound(String),
 }
 
 /// A container ID.
@@ -133,148 +158,263 @@ impl fmt::Display for ContainerInfo {
 }
 
 impl ContainerInfo {
-    pub fn from_container_id(id: ContainerId) -> Result<Self, ContainerError> {
-        match id {
-            ContainerId::Docker(id) => {
-                let path = PathBuf::from(DOCKER_CONTAINERS_PATH)
-                    .join(&id)
-                    .join("config.v2.json");
-                let file = File::open(&path).map_err(|source| ContainerError::ReadFile {
-                    source,
-                    path: path.clone(),
-                })?;
+    pub fn from_pid(pid: Pid) -> Result<Option<Self>, ContainerError> {
+        let Some(container_id) = procfs::get_process_container_id(pid)? else {
+            return Ok(None);
+        };
 
-                let reader = BufReader::new(file);
-                let config: DockerConfig = serde_json::from_reader(reader)
-                    .map_err(|source| ContainerError::ParseConfigFile { source, path })?;
+        let info = match container_id {
+            ContainerId::Docker(id) => Self::from_docker_id(id),
+            ContainerId::Libpod(id) => Self::from_libpod_id(id, pid),
+        };
 
-                let name = config.name;
-                let name = if let Some(name) = name.strip_prefix('/') {
-                    name.to_owned()
-                } else {
-                    name
-                };
-                let image = config.config.image;
-                let image_digest = config.image_digest;
-
-                Ok(Self {
-                    id,
-                    name,
-                    image,
-                    image_digest,
-                })
-            }
-            ContainerId::Libpod(container_id) => {
-                let (db_config, db_path, image_store_path) = find_libpod_container(&container_id)?;
-                let config: LibpodConfig =
-                    serde_json::from_str(&db_config.json).map_err(|source| {
-                        ContainerError::ParseConfigDB {
-                            source,
-                            path: db_path,
-                        }
-                    })?;
-                let image_id = config.rootfs_image_id;
-
-                let image_store_file =
-                    File::open(&image_store_path).map_err(|source| ContainerError::ReadFile {
-                        source,
-                        path: image_store_path.clone(),
-                    })?;
-                let reader = BufReader::new(image_store_file);
-
-                let images: Vec<LibpodImageConfig> =
-                    serde_json::from_reader(reader).map_err(|source| {
-                        ContainerError::ParseConfigFile {
-                            source,
-                            path: image_store_path.clone(),
-                        }
-                    })?;
-                let image = images.iter().find(|image| image.id == image_id).ok_or(
-                    ContainerError::ImageNotFound {
-                        id: image_id,
-                        path: image_store_path,
-                    },
-                )?;
-
-                Ok(Self {
-                    id: container_id,
-                    name: config.name,
-                    image: config.rootfs_image_name,
-                    image_digest: image.digest.clone(),
-                })
-            }
-        }
-    }
-}
-
-fn find_libpod_container(
-    container_id: &str,
-) -> Result<(LibpodDBContainerConfig, PathBuf, PathBuf), ContainerError> {
-    for (db_path, image_store_path) in find_existing_libpod_files()? {
-        if let Some(container) = find_libpod_container_in_db(&container_id, &db_path)? {
-            return Ok((container, db_path, image_store_path));
-        }
+        info.map(Some)
     }
 
-    Err(ContainerError::ContainerNotFound {
-        id: container_id.to_owned(),
-    })
-}
+    fn from_docker_id(id: String) -> Result<Self, ContainerError> {
+        const DOCKER_CONTAINERS_PATH: &str = "/var/lib/docker/containers";
 
-fn find_existing_libpod_files() -> Result<Vec<(PathBuf, PathBuf)>, ContainerError> {
-    let mut db_files = Vec::new();
+        let path = PathBuf::from(DOCKER_CONTAINERS_PATH)
+            .join(&id)
+            .join("config.v2.json");
+        let file = File::open(&path).map_err(|source| ContainerError::ReadFile {
+            source,
+            path: path.clone(),
+        })?;
 
-    let db_root_path = PathBuf::from(LIBPOD_DB_ROOT_PATH);
-    let image_store_root_path = PathBuf::from(LIBPOD_IMAGE_STORE_PATH);
-    if db_root_path.exists() && image_store_root_path.exists() {
-        db_files.push((db_root_path, image_store_root_path));
+        let reader = BufReader::new(file);
+        let config: DockerConfig = serde_json::from_reader(reader)
+            .map_err(|source| ContainerError::ParseConfigFile { source, path })?;
+
+        let name = config.name;
+        let name = if let Some(name) = name.strip_prefix('/') {
+            name.to_owned()
+        } else {
+            name
+        };
+        let image = config.config.image;
+        let image_digest = config.image_digest;
+
+        Ok(Self {
+            id,
+            name,
+            image,
+            image_digest,
+        })
     }
 
-    let file = File::open(ETC_PASSWD).map_err(|source| ContainerError::ReadFile {
-        source,
-        path: PathBuf::from(ETC_PASSWD),
-    })?;
-    let reader = BufReader::new(file);
+    fn from_libpod_id(id: String, pid: Pid) -> Result<Self, ContainerError> {
+        let uid = procfs::get_process_user_id(pid)?;
 
-    for line in reader.lines() {
-        let line = line.unwrap();
-        if let Some(home_dir) = get_home_directory(&line) {
-            let db_path = PathBuf::from(&home_dir).join(".local/share/containers/storage/db.sql");
-            if db_path.exists() {
-                let image_store_path = PathBuf::from(home_dir)
-                    .join(".local/share/containers/storage/overlay-images/images.json");
-                if !image_store_path.exists() {
-                    continue;
+        let user_home =
+            unsafe { get_user_home_dir(uid) }.ok_or(ContainerError::HomeDirNotFound { uid })?;
+
+        let user_home = Path::new(&user_home);
+
+        let libpod_database_backend = LibpodDatabaseBackend::of_user(uid, &user_home)?;
+
+        let config = match libpod_database_backend {
+            LibpodDatabaseBackend::Auto => {
+                // From container.conf docs:
+                //
+                // The database backend of Podman.  Supported values are "" (default), "boltdb"
+                // and "sqlite". An empty value means it will check whenever a boltdb already
+                // exists and use it when it does, otherwise it will use sqlite as default
+                // (e.g. new installs). This allows for backwards compatibility with older versions.
+                // Please run `podman-system-reset` prior to changing the database
+                // backend of an existing deployment, to make sure Podman can operate correctly.
+                match boltdb_find_libpod_container_config(&id, uid, user_home) {
+                    Ok(boltdb_config) => boltdb_config,
+                    Err(ContainerError::LibpodDBNotFound(_)) => {
+                        sqlite_find_libpod_container_config(&id, uid, user_home)?
+                    }
+                    Err(e) => return Err(e),
                 }
-
-                db_files.push((db_path, image_store_path));
             }
+            LibpodDatabaseBackend::Sqlite => {
+                sqlite_find_libpod_container_config(&id, uid, user_home)?
+            }
+            LibpodDatabaseBackend::BoltDB => {
+                boltdb_find_libpod_container_config(&id, uid, user_home)?
+            }
+        };
+
+        let image_store_path =
+            find_image_store(uid, user_home).ok_or(ContainerError::ImageStoreNotFound)?;
+
+        let image_store_file =
+            File::open(&image_store_path).map_err(|source| ContainerError::ReadFile {
+                source,
+                path: image_store_path.clone(),
+            })?;
+        let reader = BufReader::new(image_store_file);
+
+        let images: Vec<LibpodImageConfig> =
+            serde_json::from_reader(reader).map_err(|source| ContainerError::ParseConfigFile {
+                source,
+                path: image_store_path.clone(),
+            })?;
+
+        let image_id = config.rootfs_image_id;
+
+        let image = images.iter().find(|image| image.id == image_id).ok_or(
+            ContainerError::ImageNotFound {
+                id: image_id,
+                path: image_store_path,
+            },
+        )?;
+
+        Ok(Self {
+            id,
+            name: config.name,
+            image: config.rootfs_image_name,
+            image_digest: image.digest.clone(),
+        })
+    }
+}
+
+#[derive(Debug)]
+enum LibpodDatabaseBackend {
+    Auto,
+    Sqlite,
+    BoltDB,
+}
+
+impl LibpodDatabaseBackend {
+    /// From container.conf docs:
+    ///
+    /// Please refer to containers.conf(5) for details of all configuration options.
+    /// Not all container engines implement all of the options.
+    /// All of the options have hard coded defaults and these options will override
+    /// the built in defaults. Users can then override these options via the command
+    /// line. Container engines will read containers.conf files in up to three
+    /// locations in the following order:
+    ///  1. /usr/share/containers/containers.conf
+    ///  2. /etc/containers/containers.conf
+    ///  3. $HOME/.config/containers/containers.conf (Rootless containers ONLY)
+    ///
+    /// Items specified in the latter containers.conf, if they exist, override the
+    /// previous containers.conf settings, or the default settings.
+    ///
+    /// So we are reading the configuration from the latter to the first
+    fn of_user<P: AsRef<Path>>(uid: Uid, user_home: P) -> Result<Self, ContainerError> {
+        if !uid.is_root() {
+            let user_container_conf = user_home
+                .as_ref()
+                .join(".config")
+                .join("containers")
+                .join("containers.conf");
+
+            if user_container_conf.exists() {
+                if let Some(db_backend) = Self::from_config(user_container_conf)? {
+                    return Ok(db_backend);
+                }
+            }
+        }
+
+        const ETC_CONTAINER_CONF: &str = "/etc/containers/containers.conf";
+
+        let etc_container_conf = Path::new(ETC_CONTAINER_CONF);
+
+        if etc_container_conf.exists() {
+            if let Some(db_backend) = Self::from_config(etc_container_conf)? {
+                return Ok(db_backend);
+            }
+        }
+
+        const USR_CONTAINER_CONF: &str = "/usr/share/containers/containers.conf";
+
+        let usr_container_conf = Path::new(USR_CONTAINER_CONF);
+
+        if !usr_container_conf.exists() {
+            return Ok(Self::Auto);
+        }
+
+        match Self::from_config(usr_container_conf)? {
+            Some(db_backend) => Ok(db_backend),
+            None => Ok(Self::Auto),
         }
     }
 
-    Ok(db_files)
-}
+    fn from_config<P: AsRef<Path>>(config_file: P) -> Result<Option<Self>, ContainerError> {
+        let container_conf = Ini::load_from_file(&config_file).map_err(|source| {
+            ContainerError::LibpodConfParsing {
+                source,
+                path: config_file.as_ref().to_owned(),
+            }
+        })?;
 
-fn get_home_directory(passwd_line: &str) -> Option<String> {
-    let parts: Vec<&str> = passwd_line.split(':').collect();
-    match parts.get(5) {
-        Some(&"/") | Some(&"/root") => None,
-        Some(home_dir) => Some(home_dir.to_string()),
-        None => None,
+        match container_conf.general_section().get("database_backend") {
+            Some("sqlite") => Ok(Some(Self::Sqlite)),
+            Some("boltdb") => Ok(Some(Self::BoltDB)),
+            Some(database_backend) => Err(ContainerError::UnknownLibpodDatabase(
+                database_backend.to_string(),
+            )),
+            None => Ok(None),
+        }
     }
 }
 
-fn find_libpod_container_in_db(
+unsafe fn get_user_home_dir(uid: Uid) -> Option<OsString> {
+    let amt = match libc::sysconf(libc::_SC_GETPW_R_SIZE_MAX) {
+        n if n < 0 => 512 as usize,
+        n => n as usize,
+    };
+    let mut buf = Vec::with_capacity(amt);
+    let mut passwd: libc::passwd = mem::zeroed();
+    let mut result = ptr::null_mut();
+    match libc::getpwuid_r(
+        uid.as_raw(),
+        &mut passwd,
+        buf.as_mut_ptr(),
+        buf.capacity(),
+        &mut result,
+    ) {
+        0 if !result.is_null() => {
+            let ptr = passwd.pw_dir as *const _;
+            let bytes = CStr::from_ptr(ptr).to_bytes();
+            if bytes.is_empty() {
+                None
+            } else {
+                Some(OsStringExt::from_vec(bytes.to_vec()))
+            }
+        }
+        _ => None,
+    }
+}
+
+fn sqlite_find_libpod_container_config<P: AsRef<Path>>(
     container_id: &str,
-    db_path: &PathBuf,
-) -> Result<Option<LibpodDBContainerConfig>, ContainerError> {
+    uid: Uid,
+    user_home: P,
+) -> Result<LibpodConfig, ContainerError> {
+    const LIBPOD_SQLITE_ROOT_PATH: &str = "/var/lib/containers/storage/db.sql";
+
+    let db_path = if uid.is_root() {
+        PathBuf::from(LIBPOD_SQLITE_ROOT_PATH)
+    } else {
+        user_home
+            .as_ref()
+            .join(".local")
+            .join("share")
+            .join("containers")
+            .join("storage")
+            .join("db.sql")
+    };
+
+    if !db_path.exists() {
+        return Err(ContainerError::LibpodDBNotFound(
+            db_path.display().to_string(),
+        ));
+    }
+
     use schema::libpod_db_container_config::dsl::*;
 
     let db_path_str = db_path.to_str().ok_or(ContainerError::PathNonUtf8 {
         path: db_path.clone(),
     })?;
     let mut conn = SqliteConnection::establish(db_path_str).map_err(|source| {
-        ContainerError::DatabaseConnection {
+        ContainerError::SqliteConnection {
             source,
             path: db_path.to_owned(),
         }
@@ -286,7 +426,95 @@ fn find_libpod_container_in_db(
         .select(LibpodDBContainerConfig::as_select())
         .first(&mut conn)
     {
-        Ok(config) => Ok(Some(config)),
-        Err(_) => Ok(None),
+        Ok(config) => {
+            let config: LibpodConfig = serde_json::from_str(&config.json).map_err(|source| {
+                ContainerError::ParseConfigDB {
+                    source,
+                    path: db_path,
+                }
+            })?;
+
+            Ok(config)
+        }
+        Err(_) => Err(ContainerError::ContainerNotFound {
+            id: container_id.to_owned(),
+        }),
     }
+}
+
+fn boltdb_find_libpod_container_config<P: AsRef<Path>>(
+    container_id: &str,
+    uid: Uid,
+    user_home: P,
+) -> Result<LibpodConfig, ContainerError> {
+    const LIBPOD_BOLTDB_ROOT_PATH: &str = "/var/lib/containers/storage/libpod/bolt_state.db";
+
+    let db_path = if uid.is_root() {
+        PathBuf::from(LIBPOD_BOLTDB_ROOT_PATH)
+    } else {
+        user_home
+            .as_ref()
+            .join(".local")
+            .join("share")
+            .join("containers")
+            .join("storage")
+            .join("libpod")
+            .join("bolt_state.db")
+    };
+
+    if !db_path.exists() {
+        return Err(ContainerError::LibpodDBNotFound(
+            db_path.display().to_string(),
+        ));
+    }
+
+    let db = nut::DBBuilder::new(&db_path)
+        .read_only(true)
+        .build()
+        .map_err(|_| ContainerError::BoltDBOpenFailed(db_path.display().to_string()))?;
+
+    let tx = db.begin_tx().unwrap();
+
+    let ctr = tx
+        .bucket(b"ctr")
+        .map_err(|_| ContainerError::BoltBucketNotFound("ctr".to_string()))?;
+
+    let container_associated_bucket = ctr
+        .bucket(container_id.as_bytes())
+        .ok_or(ContainerError::BoltBucketNotFound(container_id.to_string()))?;
+
+    let config = container_associated_bucket
+        .get(b"config")
+        .ok_or(ContainerError::BoltKeyNotFound("config".to_string()))?;
+
+    let config: LibpodConfig =
+        serde_json::from_slice(config).map_err(|source| ContainerError::ParseConfigDB {
+            source,
+            path: db_path,
+        })?;
+
+    Ok(config)
+}
+
+fn find_image_store<P: AsRef<Path>>(uid: Uid, user_home: P) -> Option<PathBuf> {
+    const LIBPOD_IMAGE_STORE_PATH: &str = "/var/lib/containers/storage/overlay-images/images.json";
+
+    let image_store_path = if uid.is_root() {
+        PathBuf::from(LIBPOD_IMAGE_STORE_PATH)
+    } else {
+        user_home
+            .as_ref()
+            .join(".local")
+            .join("share")
+            .join("containers")
+            .join("storage")
+            .join("overlay-images")
+            .join("images.json")
+    };
+
+    if !image_store_path.exists() {
+        return None;
+    }
+
+    Some(image_store_path)
 }
