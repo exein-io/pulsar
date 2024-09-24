bf759730f105ef82527dd7beaa108b4882518972
banditopazzo <banditopazzo@gmail.com>
2024-09-24 19:25:29 +0200

    feat: better xtask run as sudo command

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,10 +1,9 @@
 use clap::Parser;
-use run::run_with_sudo;
 use signal_hook::{consts::TERM_SIGNALS, iterator::Signals};
+use xtask::surun::SuRunCommand;
 
-mod run;
 mod tempdir;
-mod test;
+mod test_suite;
 mod vmlinux;
 
 #[derive(Debug, Parser)]
@@ -16,14 +15,11 @@ pub struct Options {
 
 #[derive(Debug, Parser)]
 enum Command {
-    /// Run pulsar daemon with admin privileges
-    Pulsard(run::Options),
-    /// Run pulsar cli with admin privileges
-    Pulsar(run::Options),
-    /// Run a single module with admin privileges
-    Probe(run::Options),
+    /// Same as `cargo run` but with admin privileges (using `sudo -E` as runner)
+    #[clap(name = "surun")]
+    SuRun(SuRunCommand),
     /// Run eBPF test suite with admin privileges
-    Test(test::Options),
+    TestSuite(test_suite::Options),
     /// Build headers with BTF type definitions.
     Vmlinux(vmlinux::Options),
 }
@@ -31,14 +27,14 @@ enum Command {
 fn main() {
     let opts = Options::parse();
 
+    env_logger::init();
+
     // Drop term signals: register a handler, but never check it
     let _ = Signals::new(TERM_SIGNALS).expect("error setting signal handler");
 
     let ret = match opts.command {
-        Command::Pulsard(opts) => run_with_sudo("pulsar-exec", &["pulsard"], opts),
-        Command::Pulsar(opts) => run_with_sudo("pulsar-exec", &["pulsar"], opts),
-        Command::Probe(opts) => run_with_sudo("probe", &[], opts),
-        Command::Test(opts) => test::run(opts),
+        Command::SuRun(cmd) => cmd.run(),
+        Command::TestSuite(opts) => test_suite::run(opts),
         Command::Vmlinux(opts) => vmlinux::run(opts),
     };
 
---
+++ xtask/src/surun.rs
@@ -0,0 +1,71 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use xshell::{cmd, Shell};
+
+#[derive(Debug, Parser)]
+pub struct SuRunCommand {
+    /// Arguments normally passed to `cargo run`
+    #[clap(name = "ARGS", allow_hyphen_values = true)]
+    pub run_args: Vec<String>,
+}
+
+impl SuRunCommand {
+    pub fn run(&self) -> Result<()> {
+        let cargo = std::env::var("CARGO").unwrap();
+
+        let target_triple = match self
+            .run_args
+            .iter()
+            .skip_while(|arg| *arg != "--target")
+            .nth(1) // skip the `--target` identifier
+        {
+            Some(target_triple) => target_triple.to_owned(),
+            None => get_default_target(&cargo)
+                .context(format!("failed to get target triple with {cargo}"))?,
+        };
+
+        log::debug!("Detected host triple: {target_triple}");
+
+        let target_triple_env_runner = {
+            let tt_env_format = target_triple.to_uppercase().replace("-", "_");
+            format!("CARGO_TARGET_{tt_env_format}_RUNNER")
+        };
+
+        log::debug!("Overriding env variable: {target_triple_env_runner}");
+
+        let sh = Shell::new()?;
+
+        sh.set_var(target_triple_env_runner, "sudo -E");
+
+        let args = &self.run_args;
+
+        cmd!(sh, "{cargo} run {args...}").run()?;
+
+        Ok(())
+    }
+}
+
+/// Calls `cargo -vV`` in a subprocess and returns the default Clang target triple.
+fn get_default_target(cargo_path: &str) -> Result<String> {
+    /// The [`rustc`][1] output field name that shows the target.
+    ///
+    /// [1]: https://doc.rust-lang.org/rustc/what-is-rustc.html
+    const TARGET_FIELD: &str = "host: ";
+
+    // Query rustc for defaults.
+    let output = std::process::Command::new(cargo_path)
+        .arg("-vV")
+        .output()
+        .context(format!("failed to execute `{cargo_path} -vV`"))?;
+
+    // Decode stdout.
+    let stdout = std::str::from_utf8(&output.stdout).context("failed to read stdout into uft8")?;
+
+    // Parse the default target from stdout.
+    stdout
+        .lines()
+        .find(|l| l.starts_with(TARGET_FIELD))
+        .map(|l| &l[TARGET_FIELD.len()..])
+        .context(format!("failed to parse target from {cargo_path} output"))
+        .map(str::to_owned)
+}
